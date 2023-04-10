055e99b808471e3980f7afc6e3676b64bc3b364a
mihai10001 <rvivits@gmail.com>
2023-04-11 02:52:35 +0300

    prepare canvas

--- src/components/MyCanvas.js
+++ src/components/MyCanvas.js
@@ -1,2 +1,20 @@
+import * as React from 'react';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+
+import '../App.css';
+
 export function MyCanvas() {
+  // The animation will run between the range (0, 20), which will also be the distance to objects
+  const distanceStart = 0, distanceEnd = 20;
+
+  return (
+    <>
+      <div className='canvas-as-background'>
+        <Suspense fallback={null}>
+          </Canvas>
+        </Suspense>
+      </div>
+    </>
+  );
 }
