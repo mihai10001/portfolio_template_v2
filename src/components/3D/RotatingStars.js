3b7a3d9d28d07bc6deae16a100427e35a477e049
mihai10001 <rvivits@gmail.com>
2023-04-25 22:04:28 +0300

    rotate stars with useFrame hook

--- src/components/3D/RotatingStars.js
+++ src/components/3D/RotatingStars.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useFrame } from '@react-three/fiber';
 import { Stars } from '@react-three/drei';
 
 export default React.memo(function RotatingStars({
@@ -6,7 +7,15 @@ export default React.memo(function RotatingStars({
   rotation,
   rotationSpeed,
 }) {
+  const starsRef = React.useRef();
+
+  useFrame((_, dt) => {
+    starsRef.current.rotation.y += dt / rotationSpeed;
+  });
+
   return (
-    <Stars radius={1} depth={4} count={300} factor={0.6} saturation={0} speed={2} fade />
+    <group ref={starsRef} position={position} rotation={rotation}>
+      <Stars radius={1} depth={4} count={300} factor={0.6} saturation={0} speed={2} fade />
+    </group>
   );
 });
