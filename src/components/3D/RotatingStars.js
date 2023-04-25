09f66ca1061a448c07fef4825eb1849fb0dce41c
mihai10001 <rvivits@gmail.com>
2023-04-25 22:06:21 +0300

    animate the stars to pop out on mount

--- src/components/3D/RotatingStars.js
+++ src/components/3D/RotatingStars.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Stars } from '@react-three/drei';
+import { config } from '@react-spring/web';
+import { useSpring, a } from '@react-spring/three';
 
 export default React.memo(function RotatingStars({
   position,
@@ -8,14 +10,20 @@ export default React.memo(function RotatingStars({
   rotationSpeed,
 }) {
   const starsRef = React.useRef();
+  const { aScale } = useSpring({
+    from: { aScale: 0 },
+    to: { aScale: 1 },
+    config: config.wobbly,
+    duration: 500
+  });
 
   useFrame((_, dt) => {
     starsRef.current.rotation.y += dt / rotationSpeed;
   });
 
   return (
-    <group ref={starsRef} position={position} rotation={rotation}>
+    <a.group ref={starsRef} position={position} scale={aScale} rotation={rotation}>
       <Stars radius={1} depth={4} count={300} factor={0.6} saturation={0} speed={2} fade />
-    </group>
+    </a.group>
   );
 });
