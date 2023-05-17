bce09996892330e9d9091f6e90a21e372ed07c86
mihai10001 <rvivits@gmail.com>
2023-05-17 20:42:38 +0300

    create spring based on distance (animation)

--- src/components/MyCanvas.js
+++ src/components/MyCanvas.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { useSpring, config } from '@react-spring/three';
 import Dolly from './3D/Dolly';
 import CanvasContent from './3D/CanvasContent';
 
@@ -10,6 +11,10 @@ export function MyCanvas() {
   // The animation will run between the range (0, 20), which will also be the distance to objects
   const distanceStart = 0, distanceEnd = 20;
   const [distance, setDistance] = React.useState(distanceStart);
+  const [springs, springsApi] = useSpring(() => ({
+    distance: distanceStart,
+  }), []);
+
 
   return (
     <>
@@ -22,7 +27,7 @@ export function MyCanvas() {
             camera={{ position: [-5, 0, 5], fov: 100, near: 0.1, far: 30 }}
           >
             <Dolly distance={distance} />
-            <CanvasContent />
+            <CanvasContent spring={springs.distance} />
             <color attach='background' args={[255, 255, 255]} />
           </Canvas>
         </Suspense>
