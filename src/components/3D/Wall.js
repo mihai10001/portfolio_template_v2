6821c72c6bd8f7e271804bd9fa8e9732621f72d2
mihai10001 <rvivits@gmail.com>
2023-05-17 20:15:00 +0300

    animate rotation of wall based on the spring value

--- src/components/3D/Wall.js
+++ src/components/3D/Wall.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { DoubleSide } from 'three';
+import { a } from '@react-spring/three';
 
 export default function Wall({
   size,
@@ -8,15 +9,24 @@ export default function Wall({
   end,
   positionStart,
   positionEnd,
+  rotationStart,
+  rotationEnd,
 }) {
   const aPositionX = spring.to([0, start, end, 20], [positionStart.x, positionStart.x, positionEnd.x, positionEnd.x]);
   const aPositionY = spring.to([0, start, end, 20], [positionStart.y, positionStart.y, positionEnd.y, positionEnd.y]);
   const aPositionZ = spring.to([0, start, end, 20], [positionStart.z, positionStart.z, positionEnd.z, positionEnd.z]);
+  const aRotationX = spring.to([0, start, end, 20], [rotationStart.x, rotationStart.x, rotationEnd.x, rotationEnd.x]);
+  const aRotationY = spring.to([0, start, end, 20], [rotationStart.y, rotationStart.y, rotationEnd.y, rotationEnd.y]);
+  const aRotationZ = spring.to([0, start, end, 20], [rotationStart.z, rotationStart.z, rotationEnd.z, rotationEnd.z]);
+
   return (
     <a.mesh
       position-x={aPositionX}
       position-y={aPositionY}
       position-z={aPositionZ}
+      rotation-x={aRotationX}
+      rotation-y={aRotationY}
+      rotation-z={aRotationZ}
       receiveShadow>
       <planeGeometry args={[size, size]} />
       <meshStandardMaterial color={"white"} side={DoubleSide} />
