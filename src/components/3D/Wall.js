7a4fd5a8d9c91ddf0c92f5ef7407c6c7a2c5c817
mihai10001 <rvivits@gmail.com>
2023-05-17 20:14:12 +0300

    animate position of wall based on the spring value

--- src/components/3D/Wall.js
+++ src/components/3D/Wall.js
@@ -3,11 +3,23 @@ import { DoubleSide } from 'three';
 
 export default function Wall({
   size,
+  spring,
+  start,
+  end,
+  positionStart,
+  positionEnd,
 }) {
+  const aPositionX = spring.to([0, start, end, 20], [positionStart.x, positionStart.x, positionEnd.x, positionEnd.x]);
+  const aPositionY = spring.to([0, start, end, 20], [positionStart.y, positionStart.y, positionEnd.y, positionEnd.y]);
+  const aPositionZ = spring.to([0, start, end, 20], [positionStart.z, positionStart.z, positionEnd.z, positionEnd.z]);
   return (
-    <mesh receiveShadow>
+    <a.mesh
+      position-x={aPositionX}
+      position-y={aPositionY}
+      position-z={aPositionZ}
+      receiveShadow>
       <planeGeometry args={[size, size]} />
       <meshStandardMaterial color={"white"} side={DoubleSide} />
-    </mesh>
+    </a.mesh>
   );
 };
