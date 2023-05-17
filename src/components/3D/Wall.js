921eb9f9f833b2c3af1248abae82652cabe20f95
mihai10001 <rvivits@gmail.com>
2023-05-17 20:15:49 +0300

    animate wall scale (relative size)

--- src/components/3D/Wall.js
+++ src/components/3D/Wall.js
@@ -11,10 +11,13 @@ export default function Wall({
   positionEnd,
   rotationStart,
   rotationEnd,
+  scaleStart = 0,
+  scaleEnd = 1,
 }) {
   const aPositionX = spring.to([0, start, end, 20], [positionStart.x, positionStart.x, positionEnd.x, positionEnd.x]);
   const aPositionY = spring.to([0, start, end, 20], [positionStart.y, positionStart.y, positionEnd.y, positionEnd.y]);
   const aPositionZ = spring.to([0, start, end, 20], [positionStart.z, positionStart.z, positionEnd.z, positionEnd.z]);
+  const aScale     = spring.to([0, start, end, 20], [scaleStart, scaleStart, scaleEnd, scaleEnd]);
   const aRotationX = spring.to([0, start, end, 20], [rotationStart.x, rotationStart.x, rotationEnd.x, rotationEnd.x]);
   const aRotationY = spring.to([0, start, end, 20], [rotationStart.y, rotationStart.y, rotationEnd.y, rotationEnd.y]);
   const aRotationZ = spring.to([0, start, end, 20], [rotationStart.z, rotationStart.z, rotationEnd.z, rotationEnd.z]);
@@ -24,6 +27,7 @@ export default function Wall({
       position-x={aPositionX}
       position-y={aPositionY}
       position-z={aPositionZ}
+      scale={aScale}
       rotation-x={aRotationX}
       rotation-y={aRotationY}
       rotation-z={aRotationZ}
