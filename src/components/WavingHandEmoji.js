de451b8f1b21098129cc92b6f3d55beb5b4d10d7
mihai10001 <rvivits@gmail.com>
2023-07-26 06:42:20 +0300

    simple waving hand emoji component

---
+++ src/components/WavingHandEmoji.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default React.memo(function WavingHandEmoji() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <span>
+        👋🏻
+      </span>
+    </div>
+  );
+});
