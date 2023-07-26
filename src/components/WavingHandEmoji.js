1fece7dd4aee48b092b80e829f566acc848f41bc
mihai10001 <rvivits@gmail.com>
2023-07-26 06:44:30 +0300

    animate hand to wave

--- src/components/WavingHandEmoji.js
+++ src/components/WavingHandEmoji.js
@@ -1,11 +1,35 @@
 import * as React from 'react';
+import { useSpring, animated, to, config } from '@react-spring/web';
 
 export default React.memo(function WavingHandEmoji() {
+  const { theta, scale } = useSpring({
+    from: { theta: 0, scale: 0 },
+    to: async (next) => {
+      let counter = 0;
+      while (counter < 2) {
+        await next({ theta: Math.PI / 8, scale: 1 });
+        await next({ theta: -Math.PI / 8, scale: 1 });
+        counter++;
+      }
+      await next({ theta: 0, scale: 0 });
+    },
+    config: { ...config.wobbly, duration: 150 },
+    delay: 3000,
+    loop: true,
+  });
+
+  const transformValue = (theta, scale) => `rotate(${theta}rad) scale(${scale})`;
+
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
-      <span>
+      <animated.span
+        style={{
+          fontSize: "3rem",
+          transform: to([theta, scale], transformValue)
+        }}
+      >
         👋🏻
-      </span>
+      </animated.span>
     </div>
   );
 });
